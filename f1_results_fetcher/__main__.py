93bea006ca9e9b94c6b865ce26f77938b832f486
lovelofstrom <lovelofstroem@gmail.com>
2022-10-06 18:25:36 +0200

    added cli args. enabled appending of results file. as well as specifying the destination file.

--- f1_results_fetcher/__main__.py
+++ f1_results_fetcher/__main__.py
@@ -1,26 +1,52 @@
+import argparse
 from pathlib import Path
 from f1_results_fetcher import get_race_results
 
 
-# default project directory for results to be stored.
-# it might make sense to take the url as a command line argument?
-RESULTS_PATH = Path(".").absolute() / "data"
+LATEST_F1_RESULTS_URL = "https://ergast.com/api/f1/current/last/results.json"
+DEFAULT_RESULTS_PATH = Path(__file__).absolute().parent.parent / "data"
+
+
+def get_default_file_name(df, file_ext: str = ".csv") -> Path:
+    """
+    Creates a file path based on the names of the first value in the
+    first two columns of the pd.DataFrame.
+    """
+    part_a, part_b = df.iloc[0, 0:2]
+    return DEFAULT_RESULTS_PATH / f"{part_a}{part_b}{file_ext}"
 
 
 if __name__ == "__main__":
-    df = get_race_results()
-    if df.empty:
-        print("No results data.")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--url",
+        help="Results url",
+        default=LATEST_F1_RESULTS_URL,
+    )
+    parser.add_argument(
+        "--dst",
+        help="full file dir to the output file including .csv ext",
+        default=None,
+    )
+    parser.add_argument(
+        "--save",
+        type=str.lower,
+        default="replace",
+        choices=["replace", "append"],
+        help="Replaces or appends to the results file.",
+    )
+    args = parser.parse_args()
+
+    df = get_race_results(args.url)
+
+    if args.dst is None:
+        pass
+        file_dst_path = get_default_file_name(df)
     else:
-        try:
-            # creates a file name based on the first values of the first
-            # two columns of the dataframe.
-            part_a, part_b = df.iloc[0, 0:2]
-            file_path = RESULTS_PATH / f"{part_a}{part_b}.csv"
-        except (IndexError, ValueError):
-            print("The expected columns were not found.")
-        if file_path.exists():
-            print("Data has already been fetched.")
-        else:
-            df.to_csv(file_path, index=True)
-            print(f"F1 results were saved in {file_path}")
+        file_dst_path = Path(args.dst)
+
+    if args.save == "append":
+        include_header = not file_dst_path.exists()
+        df.to_csv(file_dst_path, mode="a", header=include_header)
+    else:
+        df.to_csv(file_dst_path)
